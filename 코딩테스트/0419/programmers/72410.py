06b578f5241c9fa6ba262ae8b195f91b146e6f39
dhLeoKim <win3ctrl@gmail.com>
2024-05-21 21:56:38 +0900

    0521 PS

---
+++ 코딩테스트/0419/programmers/72410.py
@@ -0,0 +1,31 @@
+def solution(new_id):
+    char = ['-', '_', '.']
+    
+    new_id = new_id.lower()
+
+    answer = ''
+    for i in new_id:
+        if i.isalpha() or i.isdecimal() or i in char: 
+            answer += i
+
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    answer = answer.strip('.')
+
+    if answer == '': 
+        answer += 'a'
+
+    n = len(answer)
+    if n >= 16:
+        answer = answer[:15]
+        answer = answer.rstrip('.')
+
+    if n <= 2:
+        answer += answer[-1]*(3-n)
+    
+    return answer
+
+new_id = "...!@BaT#*..y.abcdefghijklm"
+
+print(solution(new_id))
