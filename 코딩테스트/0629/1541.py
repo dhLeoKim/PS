475fd0e217347a3beb3232934bb578bd52edac39
dhLeoKim <win3ctrl@gmail.com>
2024-07-20 11:58:01 +0900

    0720 PS

---
+++ 코딩테스트/0629/1541.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input_1541.txt')
+input = sys.stdin.readline
+
+lst = list(input().strip())
+lst.append('+')
+
+ret = 0
+flag = False
+num = ''
+for i in lst:
+    if i.isdigit():
+        num += i
+    else:
+        if flag:
+            ret -= int(num)
+            num = ''
+
+            continue
+        else:
+            ret += int(num)
+            num = ''
+
+        if i == '-':
+            flag = True
+
+print(ret)
