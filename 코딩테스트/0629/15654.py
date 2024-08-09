391cc688461c913a6865cb2960c757f390bb37bd
dhLeoKim <win3ctrl@gmail.com>
2024-08-09 23:10:55 +0900

    0809 PS

---
+++ 코딩테스트/0629/15654.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('input_15654.txt')
+input = sys.stdin.readline
+
+def dfs(k):
+    if k == M:
+        print(*arr)
+        return
+    
+    for i in lst:
+        if i not in arr:
+            arr.append(i)
+            dfs(k+1)
+            arr.pop()
+
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+
+arr = []
+dfs(0)
