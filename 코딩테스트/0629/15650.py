83d5a9450d639e1edad6a1f8d9798ee481417d1f
dhLeoKim <win3ctrl@gmail.com>
2024-08-11 18:59:22 +0900

    0811 PS

---
+++ 코딩테스트/0629/15650.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input_15650.txt')
+input = sys.stdin.readline
+
+def dfs(k):
+    if len(arr) == M:
+        print(*arr)
+        return
+    
+    for i in range(k, N+1):
+        if i not in arr:
+            arr.append(i)
+            dfs(i+1)
+            arr.pop()
+
+N, M = map(int, input().split())
+
+arr = []
+dfs(1)
--- 코딩테스트/0629/15655.py
+++ 코딩테스트/0629/15655.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input_15655.txt')
 input = sys.stdin.readline
 
-def backtracking(k, s):
+def dfs(k, s):
     if k == M:
         print(*arr)
         return
@@ -10,7 +10,7 @@ def backtracking(k, s):
     for i in range(s, N):
         if lst[i] not in arr:
             arr.append(lst[i])
-            backtracking(k+1, i)
+            dfs(k+1, i)
             arr.pop()
 
 N, M = map(int, input().split())
@@ -18,4 +18,4 @@ lst = list(map(int, input().split()))
 lst.sort()
 
 arr = []
-backtracking(0, 0)
+dfs(0, 0)
